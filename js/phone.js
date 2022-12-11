163893c40d792e9836ecfea905b491a4f30e08d1
sabiha-wasema <sabihawasemaa1244@gmail.com>
2022-12-11 22:05:24 +0600

    card-details added

--- js/phone.js
+++ js/phone.js
@@ -6,7 +6,9 @@ const loadPhone = () => {
   const url = ` https://openapi.programming-hero.com/api/phones?search=${searchText}`;
   let letters = /^[A-Za-z]+$/;
   if (searchField.value.match(letters)) {
-    fetch(url).then(res => res.json()).then(data => displayPhone(data.data));
+    fetch(url)
+      .then(res => res.json())
+      .then(data => displayPhone(data.data.slice(0, 20)));
   } else {
     alert("Search Field is empty or Invalid input");
   }
@@ -51,6 +53,7 @@ const showDetails = phoneId => {
 const detailContainer = id => {
   console.log(id);
   const showDetailContainer = document.getElementById("show-details");
+  showDetailContainer.textContent = "";
   const div = document.createElement("div");
   div.classList.add("detail-card");
   div.innerHTML = `
@@ -61,9 +64,20 @@ const detailContainer = id => {
             ? id.releaseDate
             : "Not Release Date Found"}</p>
         <p class="card-text"><span>Brand Name: </span> ${id.brand}</p>
-        <h5>Storage: ${id.mainFeatures.storage}</h5>
-        <p>Display: ${id.mainFeatures.displaySize}</p>
-        <p> ChipSet:  ${id.mainFeatures.chipSet}</p>
+        <h5><span>Storage:</span> ${id.mainFeatures.storage}</h5>
+        <p><span>Display:</span> ${id.mainFeatures.displaySize}</p>
+        <p><span>ChipSet: </span> ${id.mainFeatures.chipSet}</p>
+        <ul class='sensors'><span>Sensors: </span>
+        <li>${id.mainFeatures.sensors[0]},</li>
+        <li>${id.mainFeatures.sensors[1]},</li>
+        <li>${id.mainFeatures.sensors[2]},</li>
+        <li>${id.mainFeatures.sensors[3]},</li>
+        <li>${id.mainFeatures.sensors[4]},</li>
+        <li>${id.mainFeatures.sensors[5]}</li>
+        <ul>
+        <p><span>Others(Bluetooth): </span>${id.others
+          ? id.others.Bluetooth
+          : "Not Found"}</p>
       </div>
   `;
   showDetailContainer.appendChild(div);
